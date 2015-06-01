ca601e88cb58dd0f360b007891e7da66b1649f74
fdurovsky <durovsky987@gmail.com>
2015-06-01 08:58:01 +0200

    removed original driver

--- dk16xx_v2p6p0/linux-sw/host_linx/pniolib/version/siemens.h
+++
@@ -1,6 +0,0 @@
-/* SIMATIC NET-spezifische Definitionen */
-
-#define SIN_COMPANY_NAME    "Siemens AG\0"
-#define SIN_LEGALCOPYRIGHT  "Copyright (C) SIEMENS CORP., 2013 All rights reserved.
-#define SIN_LEGALTRADEMARKS "Siemens AG\0"
-#define SIN_PRODUCT_STR     "SIMATIC NET PC Software\0"
